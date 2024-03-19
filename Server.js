acf03e91787356cf84ccd4b022fbd0ccb5d6868a
Aiswarya-B-Unnithan <140697168+Aiswarya-B-Unnithan@users.noreply.github.com>
2024-03-19 18:44:40 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -11,10 +11,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(
-  cors({
-    origin: "https://todoappfrontend-six.vercel.app",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  })
+  cors()
 );
 app.use(cookieParser());
 app.use(express.json());
